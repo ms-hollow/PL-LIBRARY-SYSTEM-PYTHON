2c01baf452b080c3a651c158c4d6bbe025bce6ba
ms-hollow <leonen.abbegail@gmail.com>
2023-07-04 17:36:48 +0800

    ok na register

--- build/Register.py
+++ build/Register.py
@@ -17,7 +17,7 @@ def register():
     name = nameEntry.get()
     TUP_ID = tupidEntry.get()
     password = passwordEntry.get()
-    yearSection = "BSCS" #insert yearSection
+    yearSection = yearSectionEntry.get()
     repassword = reenterpassEntry.get()
     contactNum = contactnumEntry.get()
     email = emailEntry.get()
@@ -93,27 +93,42 @@ canvas.create_text(
 )
 
 canvas.create_text(
-    653.0,
-    124.0,
+    683.0,
+    118.0,
     anchor="nw",
     text="TUP ID",
     fill="#4B0000",
     font=font_style
 )
 
-entry_image_1 = PhotoImage(file=relative_to_assets("entry_1.png"))
-entry_bg_1 = canvas.create_image(833.5, 245.0, image=entry_image_1)
-nameEntry = Entry(
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("nameEntry.png"))
+entry_bg_1 = canvas.create_image(
+    863.5,
+    221.0,
+    image=entry_image_1
+)
+nameEntry= Entry(
     bd=0,
     bg="#FFFDFD",
     fg="#000716",
     highlightthickness=0,
     font=font.Font(family="Poppins", size=13, weight="normal")
 )
-nameEntry.place(x=664.0, y=230.0, width=339.0, height=25.0)
+nameEntry.place(
+    x=694.0,
+    y=206.0,
+    width=339.0,
+    height=23.0
+)
 
-entry_image_2 = PhotoImage(file=relative_to_assets("entry_2.png"))
-entry_bg_2 = canvas.create_image(833.5, 175.0, image=entry_image_2)
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("tupidEntry.png"))
+entry_bg_2 = canvas.create_image(
+    863.5,
+    164.0,
+    image=entry_image_2
+)
 tupidEntry = Entry(
     bd=0,
     bg="#FFFDFD",
@@ -121,38 +136,68 @@ tupidEntry = Entry(
     highlightthickness=0,
     font=font.Font(family="Poppins", size=13, weight="normal")
 )
-tupidEntry.place(x=664.0, y=160.0, width=339.0, height=25.0)
-
-canvas.create_text(652.0, 194.0, anchor="nw", text="Name", fill="#4B0000", font=font_style)
-
-entry_image_3 = PhotoImage(file=relative_to_assets("entry_3.png"))
-entry_bg_3 = canvas.create_image(833.0, 315.0, image=entry_image_3)
-passwordEntry = Entry(
-    bd=0,
-    bg="#FFFDFD",
-    fg="#000716",
-    highlightthickness=0,
-    font=font.Font(family="Poppins", size=13, weight="bold"),
-    show="*"
+tupidEntry.place(
+    x=694.0,
+    y=149.0,
+    width=339.0,
+    height=23.0
 )
-passwordEntry.place(x=664.0, y=300.0, width=338.0, height=25.0)
 
-canvas.create_text(652.0, 263.0, anchor="nw", text="Password", fill="#4B0000", font=font_style)
-canvas.create_text(653.0, 334.0, anchor="nw", text="Re-Enter Password", fill="#4B0000", font=font_style)
+canvas.create_text(
+    683.0,
+    175.0,
+    anchor="nw",
+    text="Name",
+    fill="#4B0000",
+    font=font_style
+)
 
-entry_image_4 = PhotoImage(file=relative_to_assets("entry_4.png"))
-entry_bg_4 = canvas.create_image(833.0, 455.0, image=entry_image_4)
-contactnumEntry = Entry(
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("yearSectionEntry.png"))
+entry_bg_3 = canvas.create_image(
+    862.0,
+    284.0,
+    image=entry_image_3
+)
+yearSectionEntry = Entry(
     bd=0,
     bg="#FFFDFD",
     fg="#000716",
     highlightthickness=0,
     font=font.Font(family="Poppins", size=13, weight="normal")
 )
-contactnumEntry.place(x=664.0, y=440.0, width=338.0, height=25.0)
+yearSectionEntry.place(
+    x=693.0,
+    y=269.0,
+    width=338.0,
+    height=23.0
+)
 
-entry_image_5 = PhotoImage(file=relative_to_assets("entry_5.png"))
-entry_bg_5 = canvas.create_image(833.0, 385.0, image=entry_image_5)
+canvas.create_text(
+    682.0,
+    236.0,
+    anchor="nw",
+    text="Year and Section",
+    fill="#4B0000",
+    font=font_style
+)
+
+canvas.create_text(
+    683.0,
+    299.0,
+    anchor="nw",
+    text="Password",
+    fill="#4B0000",
+    font=font_style
+)
+
+entry_image_4 = PhotoImage(
+    file=relative_to_assets("reenterpassEntry.png"))
+entry_bg_4 = canvas.create_image(
+    864.0,
+    407.0,
+    image=entry_image_4
+)
 reenterpassEntry = Entry(
     bd=0,
     bg="#FFFDFD",
@@ -161,12 +206,81 @@ reenterpassEntry = Entry(
     font=font.Font(family="Poppins", size=13, weight="bold"),
     show="*"
 )
-reenterpassEntry.place(x=664.0, y=370.0, width=338.0, height=25.0)
+reenterpassEntry.place(
+    x=695.0,
+    y=392.0,
+    width=338.0,
+    height=23.0
+)
 
-canvas.create_text(652.0, 403.0, anchor="nw", text="Contact Number", fill="#4B0000", font=font_style)
+entry_image_5 = PhotoImage(
+    file=relative_to_assets("passwordEntry.png"))
+entry_bg_5 = canvas.create_image(
+    864.0,
+    344.0,
+    image=entry_image_5
+)
+passwordEntry = Entry(
+    bd=0,
+    bg="#FFFDFD",
+    fg="#000716",
+    highlightthickness=0,
+    font=font.Font(family="Poppins", size=13, weight="bold"),
+    show="*"
+)
+passwordEntry.place(
+    x=695.0,
+    y=329.0,
+    width=338.0,
+    height=23.0
+)
 
-entry_image_6 = PhotoImage(file=relative_to_assets("entry_6.png"))
-entry_bg_6 = canvas.create_image(833.0, 525.0, image=entry_image_6)
+canvas.create_text(
+    682.0,
+    362.0,
+    anchor="nw",
+    text="Re-enter Password",
+    fill="#4B0000",
+    font=font_style
+)
+
+entry_image_6 = PhotoImage(
+    file=relative_to_assets("contactnumEntry.png"))
+entry_bg_6 = canvas.create_image(
+    864.0,
+    470.0,
+    image=entry_image_6
+)
+contactnumEntry = Entry(
+    bd=0,
+    bg="#FFFDFD",
+    fg="#000716",
+    highlightthickness=0,
+    font=font.Font(family="Poppins", size=13, weight="normal")
+)
+contactnumEntry.place(
+    x=695.0,
+    y=455.0,
+    width=338.0,
+    height=23.0
+)
+
+canvas.create_text(
+    683.0,
+    425.0,
+    anchor="nw",
+    text="Contact Number",
+    fill="#4B0000",
+    font=font_style
+)
+
+entry_image_7 = PhotoImage(
+    file=relative_to_assets("emailEntry.png"))
+entry_bg_7 = canvas.create_image(
+    864.0,
+    533.0,
+    image=entry_image_7
+)
 emailEntry = Entry(
     bd=0,
     bg="#FFFDFD",
@@ -174,20 +288,38 @@ emailEntry = Entry(
     highlightthickness=0,
     font=font.Font(family="Poppins", size=13, weight="normal")
 )
-emailEntry.place(x=664.0, y=510.0, width=338.0, height=25.0)
+emailEntry.place(
+    x=695.0,
+    y=518.0,
+    width=338.0,
+    height=23.0
+)
 
-canvas.create_text(653.0, 474.0, anchor="nw", text="Email", fill="#4B0000", font=font_style)
+canvas.create_text(
+    683.0,
+    486.0,
+    anchor="nw",
+    text="Email",
+    fill="#4B0000",
+    font=font_style
+)
 
-button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
-button_1 = Button(
+button_image_1 = PhotoImage(
+    file=relative_to_assets("registerBtn.png"))
+register = Button(
     image=button_image_1,
     borderwidth=1,
     highlightthickness=0,
     command=register,
     relief="flat",
-    bg = "#C19A6B"
+    bg="#C19A6B"
 )
-button_1.place(x=749.0, y=580.0, width=170.0, height=44.0)
+register.place(
+    x=771.0,
+    y=578.0,
+    width=170.0,
+    height=44.0
+)
 
 image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
 image_1 = canvas.create_image(349.0, 298.0, image=image_image_1)
